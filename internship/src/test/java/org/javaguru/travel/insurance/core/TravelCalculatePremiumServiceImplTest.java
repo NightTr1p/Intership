5d618d539d6a90307aff2eb6e9135b2416ed7f4a
Roman Ykovenko <romanykovenko@icloud.com>
2024-12-09 20:14:16 +0300

    STEP - 214 DONE

--- internship/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ internship/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -1,12 +1,32 @@
 package org.javaguru.travel.insurance.core;
 
+import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
+import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class TravelCalculatePremiumServiceImplTest {
 
     @Test
-    public void deleteMe() {
+    void TestTravelCalculatePremiumKonstResponse() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        request.setPersonLastName("Ykov");
+        request.setPersonFirstName("Roma");
+        request.setAgreementDateFrom(new Date());
+        request.setAgreementDateTo(new Date());
 
+        TravelCalculatePremiumServiceImpl service = new TravelCalculatePremiumServiceImpl();
+
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+
+        assertEquals(request.getPersonFirstName(), response.getPersonFirstName());
+        assertEquals(request.getPersonLastName(), response.getPersonLastName());
+        assertEquals(request.getAgreementDateFrom(), response.getAgreementDateFrom());
+        assertEquals(request.getAgreementDateTo(), response.getAgreementDateTo());
     }
 
 }
