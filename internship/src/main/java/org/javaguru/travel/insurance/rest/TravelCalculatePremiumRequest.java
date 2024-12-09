3a0cf732fce99e857137c93801ef135d2f8459aa
Roman Ykovenko <romanykovenko@icloud.com>
2024-12-09 19:23:14 +0300

    STEP 211 - DONE

--- internship/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
+++ internship/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
@@ -6,7 +6,50 @@ public class TravelCalculatePremiumRequest {
 
     private String personFirstName;
     private String personLastName;
-    private Date agreementDateFrom;
-    private Date agreementDateTo;
 
+    private Date agreementDateFrom;
+
+    private Date agreementDateTo;
+    TravelCalculatePremiumRequest() {
+
+    }
+
+    public void StartTravelCalculatePremiumRequest(String firstName, String lastName, Date from, Date to) {
+        personFirstName = firstName;
+        personLastName = lastName;
+        agreementDateFrom = from;
+        agreementDateTo = to;
+    }
+
+    public String getPersonFirstName() {
+        return personFirstName;
+    }
+
+    public void setPersonFirstName(String personFirstName) {
+        this.personFirstName = personFirstName;
+    }
+
+    public String getPersonLastName() {
+        return personLastName;
+    }
+
+    public void setPersonLastName(String personLastName) {
+        this.personLastName = personLastName;
+    }
+
+    public Date getAgreementDateFrom() {
+        return agreementDateFrom;
+    }
+
+    public void setAgreementDateFrom(Date agreementDateFrom) {
+        this.agreementDateFrom = agreementDateFrom;
+    }
+
+    public Date getAgreementDateTo() {
+        return agreementDateTo;
+    }
+
+    public void setAgreementDateTo(Date agreementDateTo) {
+        this.agreementDateTo = agreementDateTo;
+    }
 }
