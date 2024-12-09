018051c5c1a359f8888f04500169fddcc3183bf1
Roman Ykovenko <romanykovenko@icloud.com>
2024-12-09 20:52:43 +0300

    STEP - 215 DONE

--- internship/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ internship/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -12,20 +12,46 @@ import static org.junit.jupiter.api.Assertions.*;
 class TravelCalculatePremiumServiceImplTest {
 
     @Test
-    void TestTravelCalculatePremiumKonstResponse() {
+    void TestTravelCalculatePremiumResponseFirstName() {
         TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
-        request.setPersonLastName("Ykov");
-        request.setPersonFirstName("Roma");
-        request.setAgreementDateFrom(new Date());
-        request.setAgreementDateTo(new Date());
-
-        TravelCalculatePremiumServiceImpl service = new TravelCalculatePremiumServiceImpl();
-
-        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
+        String test = "roma";
+        request.setPersonFirstName(test);
+        response.setPersonFirstName(test);
 
         assertEquals(request.getPersonFirstName(), response.getPersonFirstName());
+    }
+
+    @Test
+    void TestTravelCalculatePremiumResponseLastName() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
+        String test = "ykov";
+        request.setPersonLastName(test);
+        response.setPersonLastName(test);
+
         assertEquals(request.getPersonLastName(), response.getPersonLastName());
+    }
+
+    @Test
+    void TestTravelCalculatePremiumResponseDateFrom() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
+        Date date = new Date(1212421424L);
+        request.setAgreementDateFrom(date);
+        response.setAgreementDateFrom(date);
+
         assertEquals(request.getAgreementDateFrom(), response.getAgreementDateFrom());
+    }
+
+    @Test
+    void TestTravelCalculatePremiumResponseDateTo() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
+        Date date = new Date(1212421424L);
+        request.setAgreementDateTo(date);
+        response.setAgreementDateTo(date);
+
         assertEquals(request.getAgreementDateTo(), response.getAgreementDateTo());
     }
 
